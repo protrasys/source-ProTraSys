1484d2b2c88cd512b6f6ce31bc6d3c4a559a2b43
Manav Oza <manavoza99@gmail.com>
2020-03-05 20:03:50 +0530

    Update index.js

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,25 +1,35 @@
-import React, { Fragment } from "react";
-import { Route, Switch } from "react-router-dom";
-import { FacultyLogin, FacultyControlPanel } from "../components/Faculty";
-import { StudentControlPanel, StudentLogin } from "../components/Student";
-import LandingPage from "../components/Layouts/Landing Page";
-import FileUpload from "../components/Layouts/Landing Page/FileUpload";
+import React, { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+// import StudentPrivateRouter from './PrivateRouter';
+
+import { FacultyLogin, FacultyControlPanel } from '../components/Faculty';
+import { StudentLogin } from '../components/Student';
+import LandingPage from '../components/Layouts/Landing Page';
+
+import StudentControlPanel from '../components/Student/Control Panel';
+import FileUpload from '../components/Student/Control Panel/';
+import ViewENotice from '../components/Student/Control Panel/';
 
 const Routes = () => {
   return (
     <Fragment>
       <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/facultylogin" component={FacultyLogin} />
+        <Route exact path='/' component={LandingPage} />
+        <Route exact path='/facultylogin' component={FacultyLogin} />
         <Route
           exact
-          path="/facultycontrolpanel"
+          path='/facultycontrolpanel'
           component={FacultyControlPanel}
         />
-        <Route exact path="/file" component={FileUpload} />
-        path='/studentcontrolpanel' component={StudentControlPanel}
+        <Route
+          exact
+          path='/studentcontrolpanel'
+          component={StudentControlPanel}
         />
-        <Route exact path="/studentlogin" component={StudentLogin} />
+        <Route exact path='/studentlogin' component={StudentLogin} />
+
+        <Route exact path='/fileupload' component={FileUpload} />
+        <Route exact path='/viewenotice' component={ViewENotice} />
       </Switch>
     </Fragment>
   );

92766009f9d61004e35874bb9248690164e3eb18
Anil Prajapati <51775205+anilprajapati1006@users.noreply.github.com>
2020-03-05 06:10:43 -0800

    Fac update

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,31 +1,25 @@
-import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { FacultyLogin, FacultyControlPanel } from '../components/Faculty';
-import { StudentControlPanel, StudentLogin } from '../components/Student';
-import LandingPage from '../components/Layouts/Landing Page';
-import FileUpload from '../components/Layouts/Landing Page/FileUpload';
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router-dom";
+import { FacultyLogin, FacultyControlPanel } from "../components/Faculty";
+import { StudentControlPanel, StudentLogin } from "../components/Student";
+import LandingPage from "../components/Layouts/Landing Page";
+import FileUpload from "../components/Layouts/Landing Page/FileUpload";
 
 const Routes = () => {
   return (
     <Fragment>
       <Switch>
-        <Route exact path='/' component={LandingPage} />
-        <Route exact path='/facultylogin' component={FacultyLogin} />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/facultylogin" component={FacultyLogin} />
         <Route
           exact
-          path='/facultycontrolpanel'
+          path="/facultycontrolpanel"
           component={FacultyControlPanel}
         />
-        <Route
-          exact
-          path='/file'
-          component={FileUpload}
+        <Route exact path="/file" component={FileUpload} />
+        path='/studentcontrolpanel' component={StudentControlPanel}
         />
-          path='/studentcontrolpanel'
-          component={StudentControlPanel}
-        />
-        <Route exact path='/studentlogin' component={StudentLogin} />
-
+        <Route exact path="/studentlogin" component={StudentLogin} />
       </Switch>
     </Fragment>
   );

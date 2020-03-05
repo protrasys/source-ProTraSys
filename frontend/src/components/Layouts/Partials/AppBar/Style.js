bf8477ae1d85897ee5eb985c64bfbf6c61f2339d
Manav Oza <manavoza99@gmail.com>
2020-03-05 20:02:48 +0530

    Update Style.js

--- frontend/src/components/Layouts/Partials/AppBar/Style.js
+++ frontend/src/components/Layouts/Partials/AppBar/Style.js
@@ -1,26 +1,21 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   list: {
     width: 200
   },
   padding: {
     paddingRight: 20,
-    cursor: "pointer"
+    cursor: 'pointer'
   },
   sideBarIcon: {
     padding: 0,
-    cursor: "pointer"
+    cursor: 'pointer'
   },
   appbar: {
-    border: "none"
+    border: 'none'
   },
-  Link: { textDecoration: "none" },
-  notification: {
-    position: "absolute",
-    top: "250%",
-    zIndex: "1"
-  }
+  Link: { textDecoration: 'none' }
 }));
 
 export default useStyles;

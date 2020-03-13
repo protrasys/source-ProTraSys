d208251af573bc95a573e2cf4c1a5f8d85b34729
Anil Prajapati <51775205+anilprajapati1006@users.noreply.github.com>
2020-03-12 22:03:03 -0700

    Update

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,30 +1,16 @@
-<<<<<<< HEAD
 import React, { Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
-// import StudentPrivateRouter from './PrivateRouter';
-
-import { FacultyLogin, FacultyControlPanel } from "../components/Faculty";
-import { StudentLogin } from "../components/Student";
-import LandingPage from "../components/Layouts/Landing Page";
-
-import StudentControlPanel from "../components/Student/Control Panel";
-import FileUpload from "../components/Student/Control Panel/";
-import ViewENotice from "../components/Student/Control Panel/";
-=======
-import React, { Fragment } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import { AuthServices } from '../Services';
+import { AuthServices } from "../Services";
 
 // Importing Private Routers
-import IsStudentLoggedIn from './PrivateRouters/Student/isLoggedIn';
-import IsStudentLoggedOut from './PrivateRouters/Student/isLoggedOut';
+import IsStudentLoggedIn from "./PrivateRouters/Student/isLoggedIn";
+import IsStudentLoggedOut from "./PrivateRouters/Student/isLoggedOut";
 
-import { FacultyLogin, FacultyControlPanel } from '../components/Faculty';
-import { StudentLogin, StudentControlPanel } from '../components/Student';
+import { FacultyLogin, FacultyControlPanel } from "../components/Faculty";
+import { StudentLogin, StudentControlPanel } from "../components/Student";
 
-import LandingPage from '../components/Layouts/Landing Page';
-import ForOForPage from '../components/Layouts/Landing Page/PageNotFound';
->>>>>>> master
+import LandingPage from "../components/Layouts/Landing Page";
+import ForOForPage from "../components/Layouts/Landing Page/PageNotFound";
 
 const Routes = () => {
   return (
@@ -43,20 +29,14 @@ const Routes = () => {
           component={StudentControlPanel}
           authorized={AuthServices.isAuthenticated()}
         />
-<<<<<<< HEAD
-        <Route exact path="/studentlogin" component={StudentLogin} />
 
-        <Route exact path="/fileupload" component={FileUpload} />
-        <Route exact path="/viewenotice" component={ViewENotice} />
-=======
         <IsStudentLoggedOut
           exact
-          path='/studentlogin'
+          path="/studentlogin"
           component={StudentLogin}
           authorized={AuthServices.isAuthenticated()}
         />
         <Route component={ForOForPage} />
->>>>>>> master
       </Switch>
     </Fragment>
   );
